e304a831e97a43b601d99c26b4c6c31835aff8e2
Rafael William <rafael.fim@hotmail.com>
2023-05-30 21:53:00 -0300

    add fonts

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans';
 import { Text, View } from 'react-native';
 
 export default function App() {
---
+++ src/theme/theme.ts
@@ -0,0 +1,15 @@
+export default {
+  COLORS: {
+
+  },
+  FONT_FAMILY: {
+    REGULAR: 'NunitoSans_400Regular',
+    BOLD: 'NunitoSans_700Bold'
+  },
+  FONT_SIZE: {
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24
+  }
+}
